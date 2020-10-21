cef569a89febc4676df2696c797a13c7726d01ed
Tony <jht1264044626@gmail.com>
2020-10-21 16:40:46 +0800

    keras2onnx_demo

---
+++ keras2onnx_demo.py
@@ -0,0 +1,7 @@
+import keras2onnx
+import onnx
+from keras.models import load_model
+model = load_model('model.h5')
+onnx_model = keras2onnx.convert_keras(model, model.name)
+temp_model_file = 'model.onnx'
+onnx.save_model(onnx_model, temp_model_file)
